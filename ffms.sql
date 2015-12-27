3417bfc9e8e4d2fdc2abffd991d39ab4d00fee03
Aidan Toh <zavier_mx@live.com.my>
2015-12-27 23:47:08 +0800

    Updated database structure

--- ffms.sql
+++ ffms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2015 at 07:49 PM
+-- Generation Time: Dec 27, 2015 at 04:46 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -31,21 +31,32 @@ CREATE TABLE IF NOT EXISTS `booking` (
   `uid` int(25) NOT NULL,
   `sid` int(25) NOT NULL,
   `price` double NOT NULL,
+  `date` date NOT NULL,
+  `tid` int(25) NOT NULL,
   `start` datetime NOT NULL,
   `end` datetime NOT NULL,
   `remark` text NOT NULL,
   `status` varchar(20) NOT NULL,
   PRIMARY KEY (`bid`),
   KEY `fk_sid` (`sid`),
-  KEY `fk_uid` (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  KEY `fk_uid` (`uid`),
+  KEY `fk_id` (`tid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`bid`, `uid`, `sid`, `price`, `start`, `end`, `remark`, `status`) VALUES
-(1, 1, 1, 55, '2015-12-02 04:00:00', '2015-12-02 07:00:00', 'TESTING', 'active');
+INSERT INTO `booking` (`bid`, `uid`, `sid`, `price`, `date`, `tid`, `start`, `end`, `remark`, `status`) VALUES
+(8, 1, 1, 45, '2015-12-27', 10, '2015-12-27 08:00:00', '2015-12-27 09:00:00', 'small', 'approve'),
+(9, 2, 2, 55, '2015-12-27', 11, '2015-12-27 10:00:00', '2015-12-27 11:00:00', 'medium', 'pending'),
+(10, 1, 3, 55, '2015-12-27', 12, '2015-12-27 14:00:00', '2015-12-27 15:00:00', 'big', 'approve'),
+(11, 2, 4, 60, '2015-12-28', 14, '2015-12-28 13:00:00', '2015-12-28 14:00:00', 'small a bit', 'pending'),
+(12, 1, 1, 45, '2015-12-28', 15, '2015-12-28 09:00:00', '2015-12-28 10:00:00', 'awesome', 'approve'),
+(13, 2, 2, 55, '2015-12-28', 16, '2015-12-28 15:00:00', '2015-12-28 16:00:00', 'very large', 'pending'),
+(14, 1, 3, 55, '2015-12-29', 14, '2015-12-29 08:00:00', '2015-12-29 09:00:00', 'between medium and big', 'pending'),
+(15, 2, 3, 60, '2015-12-29', 17, '2015-12-29 13:00:00', '2015-12-29 14:00:00', 'big', 'pending'),
+(16, 1, 2, 55, '2015-12-29', 18, '2015-12-29 15:00:00', '2015-12-29 16:00:00', 'small', 'pending');
 
 -- --------------------------------------------------------
 
@@ -61,14 +72,49 @@ CREATE TABLE IF NOT EXISTS `space` (
   `price` double NOT NULL,
   `status` varchar(20) NOT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `space`
 --
 
 INSERT INTO `space` (`sid`, `name`, `description`, `picPath`, `price`, `status`) VALUES
-(1, 'Court 1', 'Futsal', 'img/vinylfield.png', 35, 'active');
+(1, 'Court 1', '- Vinyl Futsal Field\r\n- Favoured field among futsal players.    Suitable for performing actions like diving.\r\n- Also known as rubber, soft field\r\n- Size: 15m X 25m (width X length)', '/localhost:8080/ffms/img/vinylfield.png', 45, 'active'),
+(2, 'Court 2', '- Vinyl Futsal Field\r\n- Favoured field among futsal players. Suitable for performing actions like diving.\r\n- Also known as rubber, soft field\r\n- Size: 18m X 38m (width X length)', '/localhost:8080/ffms/img/vinylfield.png', 55, 'active'),
+(3, 'Court 3', '- Parquette Futsal Field\r\n- Popular field in Europe and Latin America\r\n- Made of Wood\r\n- Size: 16m X 27m (width X length)', '/localhost:8080/ffms/img/paraquette.png', 55, 'active'),
+(4, 'Court 4', '- Futsal Artificial Grass\r\n- Indoor soccer with outdoor feel\r\n- Suitable for casual games\r\n- Size: 16m X 27m (width X length)', 'http://localhost:8080/ffms/img/futsal-turff.png', 60, 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time`
+--
+
+CREATE TABLE IF NOT EXISTS `time` (
+  `tid` int(25) NOT NULL,
+  `description` varchar(25) NOT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `time`
+--
+
+INSERT INTO `time` (`tid`, `description`) VALUES
+(10, '10AM'),
+(11, '11AM'),
+(12, '12PM'),
+(13, '1PM'),
+(14, '2PM'),
+(15, '3PM'),
+(16, '4PM'),
+(17, '5PM'),
+(18, '6PM'),
+(19, '7PM'),
+(20, '8PM'),
+(21, '9PM'),
+(22, '10PM'),
+(23, '11PM');
 
 -- --------------------------------------------------------
 
@@ -80,18 +126,22 @@ CREATE TABLE IF NOT EXISTS `user` (
   `uid` int(25) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(1000) NOT NULL,
   `type` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`uid`, `name`, `username`, `password`, `type`, `status`) VALUES
-(1, 'Amir', 'amir', 'amir123', 'user', 'active');
+(1, 'test', 'test@test', 'testtest', 'user', 'active'),
+(2, 'user', 'user@user', 'useruser', 'user', 'active'),
+(3, 'Aidan', 'bla@bla', 'blabka', 'user', 'active'),
+(4, 'Ahmad', 'haha@haha', 'hahaha', 'user', 'active'),
+(5, 'John', 'john@john', 'wWgApK/yacNtmLAb0PJAUdOCK4ZLl+am2GJV3T4l36rdq3wfYbEpkCKlfnnlFGBq', 'admin', 'active');
 
 --
 -- Constraints for dumped tables
@@ -101,6 +151,7 @@ INSERT INTO `user` (`uid`, `name`, `username`, `password`, `type`, `status`) VAL
 -- Constraints for table `booking`
 --
 ALTER TABLE `booking`
+  ADD CONSTRAINT `fk_id` FOREIGN KEY (`tid`) REFERENCES `time` (`tid`),
   ADD CONSTRAINT `fk_sid` FOREIGN KEY (`sid`) REFERENCES `space` (`sid`),
   ADD CONSTRAINT `fk_uid` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`);
 
